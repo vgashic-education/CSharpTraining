27f46392834ee183af61eba16fcfb68710923e4a
Vladimir Gasic <vladimir.gasic@gemikro.rs>
2021-12-24 13:31:33 +0100

    inconsistent accesibility problem example

---
+++ CS.AccesModifiers/InconsistentAccesibility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.AccesModifiers
+{
+    public class Contract
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        
+        // Customer je internal
+        // Contract je public
+        // Ako pokusamo instancirati Contract u nekom drugom projektu,
+        // necemo uspeti jer je tip jednog od property-ja deklarisan kao internal
+        // tj. ne vidi se van svog projekta (assembly-ja)
+        public Customer Customer { get; set; }
+    }
+
+    internal class Customer
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+    }
+
+}
